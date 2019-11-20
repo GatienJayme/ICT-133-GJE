c5fe237c00264bee9fe849cf7b7f43abfeb2ecef
GatienJayme <47849706+GatienJayme@users.noreply.github.com>
2019-11-20 17:09:18 +0100

    WIP

---
+++ php_demo/Index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Demo</title>
+</head>
+<body>
+<h1>MDR</h1>
+<h2>VOILA j'ai reussi mais je suis nul</h2>
+<h3>arrete de dire que je suis pas nul car je le suis</h3>
+
+<?php
+/*  Program: ICT-133-GJE_Demo_php
+    Author: Gatien Jayme
+    Date: 20.11.2019
+    Version: 1.0
+ */
+echo "Beerthe";
+?>
+
+</body>
+</html>
