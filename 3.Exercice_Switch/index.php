84e9fd5c273eff7ccab0874e9caf8395a6ec5ef3
GatienJayme <47849706+GatienJayme@users.noreply.github.com>
2019-11-28 12:15:52 +0100

    WIP des switchs

---
+++ 3.Exercice_Switch/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <script src="js/script.js"></script>
+</head>
+<body>
+
+
+<h1>SI-CMI2a</h1>
+<ul>
+    <?php
+    // TODO : Ecrire le php qui remplace le html dans la balise ul ci-dessous
+    for ($i=1; $i <= 9; $i++){
+        echo"<li id= 'div$i'>";
+
+        switch ($i) {
+            case 0:
+                echo 'Xavier';
+                break;
+            case 1:
+                echo 'Altin';
+                break;
+            case 2:
+                echo 'Samuel';
+                break;
+            case 3:
+                echo 'Chritopher';
+                break;
+            case 4:
+                echo 'Kevin';
+                break;
+            case 5:
+                echo 'Dmitri';
+                break;
+            case 6:
+                echo 'Miguel';
+                break;
+            case 7:
+                echo 'Luis';
+                break;
+            case 8:
+                echo 'Mounir';
+                break;
+            case 9:
+                echo 'Fabien';
+                break;
+        }
+
+        echo "</li>";
+    }
+
+
+    ?>
+</ul>
+
+</body>
+</html>
