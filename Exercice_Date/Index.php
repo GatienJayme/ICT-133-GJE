3ac6771929fa51581a4531977306cd5794f9ab32
GatienJayme <47849706+GatienJayme@users.noreply.github.com>
2019-11-28 11:48:17 +0100

    terminer exercice date

--- Exercice_Date/Index.php
+++ Exercice_Date/Index.php
@@ -8,6 +8,7 @@
     <title>Document</title>
 </head>
 <body>
+<h1>Code qui affiche des dates</h1>
 
 <?php
 /*  Program: Exercice Date ICT-133
@@ -16,7 +17,17 @@
     Version: 1.0
 */
 
-/*for($date = 1; $date > )*/
+$timestamp = time(); // Date exercice
+// $timestamp = strtotime('17.09.2013'); // Date automatique
+
+// liste ordonne des dates
+echo "<ol>";
+echo "<li>".date ('l, d F Y', $timestamp)."</li>";
+echo "<li>".date ('M ds Y', $timestamp)."</li>";
+echo "<li>".date ('d/m/y h:i a', $timestamp)."</li>";
+echo "<li>".date ('d M Y, H:i:s', $timestamp)."</li>";
+echo "<li>".date ('r', $timestamp)."</li>";
+echo "</ol>";
 ?>
 
 
