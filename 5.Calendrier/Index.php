64c03ce6238587a135a00f2f52b9b9494225473f
GatienJayme <47849706+GatienJayme@users.noreply.github.com>
2019-12-06 15:06:01 +0100

    WIP calendrier pas terminé mais bien commencé

--- 5.Calendrier/Index.php
+++ 5.Calendrier/Index.php
@@ -14,20 +14,33 @@
     Version: 1.0
 */
 
-$day = array("Lundi", "Mardi", "Mercredi", "Jeudi", "Samedi","Dimanche");
-$month = array ("Januar", "Februar", "March", "April", "May", "Juni", "July", "September", "October", "November", "December");
+function strstotime() {
+
+}
+
+$day = array("Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi","Dimanche");
+$month = array ("Januar", "Februar", "March","April", "May", "Juni", "July", "August",
+"September", "October", "November", "December");
 $_GET ['month'];
-$_GET ['day'];
+$_GET ['year'];
+$now = time();
 
 echo'<div class="month">';
     echo"<ul>";
-        echo"<li>August<br>2017</li>";
+        echo"<li>$month[7]<br>2017</li>";
     echo"</ul>";
 echo"</div>";
 
-for($day = 0, $day < 12, $day++) {
 
+echo '<ul class="weekdays">';
+for($weekdays = 0; $weekdays <= 7; $weekdays++) {
+    echo"<li>";
+    echo"$day[$weekdays]";
+    echo"</li>";
 }
+echo"</ul>";
+
+/*
 echo '<ul class="weekdays">';
 echo "<li>Mo</li>";
 echo "<li>Tu</li>";
@@ -36,20 +49,41 @@ echo "<li>Th</li>";
 echo "<li>Sa</li>";
 echo "<li>Su</li>";
 echo"</ul>";
+*/
 
 echo'<ul class="days">';
-   echo"<li>1</li>";
-   echo"<li>2</li>";
-   echo"<li>3</li>";
-   echo"<li>4</li>";
-   echo"<li>5</li>";
-   echo"<li>6</li>";
-   echo"<li>7</li>";
-   echo"<li>8</li>";
-   echo"<li>9</li>";
-   echo'<li><span class="active">10</span></li>';
-   echo"<li>11</li>";
+for($days = 1; $days <= 31; $days++) {
+    echo"<li>";
+    if($days == date("j",$now))
+    {
+        echo '<span class="active">'.$days.'</span>';
+    }
+    else
+    {
+        echo "$days";
+    }
+    echo"</li>";
+}
 echo"</ul>";
+
+
+
+/*
+echo'<ul class="days">';
+echo"<li>1</li>";
+echo"<li>2</li>";
+echo"<li>3</li>";
+echo"<li>4</li>";
+echo"<li>5</li>";
+echo"<li>6</li>";
+echo"<li>7</li>";
+echo"<li>8</li>";
+echo"<li>9</li>";
+echo'<li><span class="active">10</span></li>';
+echo"<li>11</li>";
+echo"</ul>";
+*/
+
 
 ?>
 
