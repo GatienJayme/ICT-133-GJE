a3eff39192f6680990e1ffc423f454102b43ad76
GatienJayme <47849706+GatienJayme@users.noreply.github.com>
2019-12-12 11:33:05 +0100

    Wip calendrier

--- 5.Calendrier/Index.php
+++ 5.Calendrier/Index.php
@@ -2,8 +2,8 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/style.css">
     <title>"My Title"</title>
-    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
@@ -14,77 +14,49 @@
     Version: 1.0
 */
 
-function strstotime() {
-
-}
-
-$day = array("Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi","Dimanche");
-$month = array ("Januar", "Februar", "March","April", "May", "Juni", "July", "August",
-"September", "October", "November", "December");
-$month = $_GET ['month'];
+// $max = strtotime('-1month', $startDate);
+$day = array("Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche");
+$month = array("Januar", "Februar", "March", "April", "May", "Juni", "July", "August",
+    "September", "October", "November", "December");
+$querymonth = $_GET ['month'];
 $year = $_GET ['year'];
 $now = time();
 
-echo'<div class="month">';
-    echo"<ul>";
-        echo"<li>$month<br>$year</li>";
-    echo"</ul>";
-echo"</div>";
+// Affichage du mois et de l'année qui change avc la query string
+echo '<div class="month">';
+echo "<ul>";
+echo "<li>$querymonth<br>$year</li>";
+echo "</ul>";
+echo "</div>";
 
-
+// Affichage des jours de la semaine
 echo '<ul class="weekdays">';
-for($weekdays = 0; $weekdays <= 7; $weekdays++) {
-    echo"<li>";
-    echo"$day[$weekdays]";
-    echo"</li>";
+for ($weekdays = 0; $weekdays <= 7; $weekdays++) {
+    echo "<li>";
+    echo "$day[$weekdays]";
+    echo "</li>";
 }
-echo"</ul>";
+echo "</ul>";
 
-/*
-echo '<ul class="weekdays">';
-echo "<li>Mo</li>";
-echo "<li>Tu</li>";
-echo "<li>We</li>";
-echo "<li>Th</li>";
-echo "<li>Sa</li>";
-echo "<li>Su</li>";
-echo"</ul>";
-*/
+// Essaier d'afficher les jours avant et après des mois
+for ($i = 1; $i <= 2; $i++) {
+    echo "$i";
+}
 
-echo'<ul class="days">';
-for($days = 1; $days <= 31; $days++) {
-    echo"<li>";
-    if($days == date("j",$now))
-    {
-        echo '<span class="active">'.$days.'</span>';
-    }
-    else
-    {
+// Affichage des dates plus la couleur du jour que l'on ait
+echo '<ul class="days">';
+for ($days = 1; $days <= 31; $days++) {
+    echo "<li>";
+    if ($days == date("j", $now)) {
+        echo '<span class="active">' . $days . '</span>';
+    } else {
         echo "$days";
     }
-    echo"</li>";
+    echo "</li>";
 }
-echo"</ul>";
-
-
-/*
-echo'<ul class="days">';
-echo"<li>1</li>";
-echo"<li>2</li>";
-echo"<li>3</li>";
-echo"<li>4</li>";
-echo"<li>5</li>";
-echo"<li>6</li>";
-echo"<li>7</li>";
-echo"<li>8</li>";
-echo"<li>9</li>";
-echo'<li><span class="active">10</span></li>';
-echo"<li>11</li>";
-echo"</ul>";
-*/
+echo "</ul>";
 
 ?>
 
 </body>
 </html>
-
