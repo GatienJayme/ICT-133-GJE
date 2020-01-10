24f29d1293be6dfd9ef42a177b2c75268a501b24
GatienJayme <47849706+GatienJayme@users.noreply.github.com>
2020-01-10 16:50:13 +0100

    WIP calender

--- 5.Calendrier/Function.php
+++ 5.Calendrier/Function.php
@@ -5,14 +5,14 @@ function getDaysBefore($querymonth, $year)
     // La fonction getDaysBefore sert a écrire le code dans une page à coté pour que le code soit plus propre
 
     // La variable firstday sert a avoir les dates des premiers jours d'un mois
-    $firstday = date("N", strtotime("-1 month", strtotime("$year-$querymonth-01")));
+    $firstday = date("N", strtotime("$year-$querymonth-01"));
 
     // La variable lastdaylastmonth sert a avoir les dates des jours de fin du mois
     $lastdaylastmonth = date("t", strtotime("-1 month", strtotime("$year-$querymonth-01")));
 
     // Cette variable sert a afficher les jours grisés des dates au début
-    for ($i = $firstday + 1; $i > 0; $i--) {
-        $daytoprint = $lastdaylastmonth - $i + 1;
+    for ($i = $firstday-1; $i > 0; $i--) {
+        $daytoprint = $lastdaylastmonth - $i +1;
         echo "<li>" . $daytoprint . "</li>";
     }
 }
@@ -29,7 +29,7 @@ function getDaysAfter($querymonth, $year)
     $nbdays = date("t", strtotime($monthdate));
 
     // La variable firstday sert a avoir les dates des premiers jours d'un mois
-    $firstday = date("N", strtotime("-1 month", strtotime("$year-$querymonth-01")));
+    $firstday = date("N",strtotime("$year-$querymonth-01"));
 
     // Affichage des dates du mois plus la couleur du jour actuel
     for ($days = 1; $days <= $nbdays; $days++) {
@@ -44,6 +44,9 @@ function getDaysAfter($querymonth, $year)
 
     // Affichage des jours après ce mois (dates grisés du début du mois)
     $nbda = 2 - (($firstday + $nbdays) % 7);
+    var_dump($nbda);
+    var_dump($firstday);
+    var_dump($nbdays);
     for ($i = 1; $i <= $nbda; $i++) {
         echo "<li>" . $i . "</li>";
     }
